3f1b3f289b143cc9d1172906f4dc55e4771f3058
BerniHarris <harrisberni@gmail.com>
2024-03-03 16:01:27 +0000

    attempt to fix seed error

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -2,8 +2,15 @@ const devData = require("../data/development-data/index.js");
 const seed = require("./seed.js");
 const db = require("../connection.js");
 
-const runSeed = () => {
-  return seed(devData).then(() => db.end());
+const runSeed = async () => {
+  try {
+    await seed(devData);
+  } catch (error) {
+    console.error("Error during seeding:", error);
+    throw error; // Re-throw the error after logging
+  } finally {
+    await db.end();
+  }
 };
 
 runSeed();
